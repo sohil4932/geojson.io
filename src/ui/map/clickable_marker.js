3b8dcc4f4b320c79d42e228b0af69f2f0ab8731b
Sohil Patel <sohilpatel4932@gmail.com>
2024-02-08 15:46:00 +0530

    changes

--- src/ui/map/clickable_marker.js
+++ src/ui/map/clickable_marker.js
@@ -5,27 +5,24 @@ class ClickableMarker extends mapboxgl.Marker {
   constructor(options, legacyOptions) {
     super(options, legacyOptions);
 
-    const {
-        symbol = "circle",
-        symbolColor = "#fff"
-    } = options
+    const { symbol = 'circle' } = options;
 
     if (
-      symbol != "circle" &&
+      symbol !== 'circle' &&
       (makiNames.includes(symbol) || /^[a-z0-9]$/.test(symbol))
     ) {
       const symbolPath = document.createElement('path');
-      this._element.querySelector('circle').replaceWith(symbolPath)
+      this._element.querySelector('circle').replaceWith(symbolPath);
 
       // download svg symbol and insert its path where the circle was
-      const xml = d3.xml(`../dist/icons/${symbol}.svg`, function(err, xml) {
-        if (err) {
-          console.error(`Error downloading the svg from: ../dist/icons/${symbol}.svg`);
-        } else {
-          symbolPath.outerHTML = `<path fill="${symbolColor}" transform="translate(6 6)"
-            d="${xml.documentElement.getElementsByTagName("path")[0].getAttribute("d")}"></path>`
-        }
-      });
+      // const xml = d3.xml(`../dist/icons/${symbol}.svg`, function(err, xml) {
+      //   if (err) {
+      //     console.error(`Error downloading the svg from: ../dist/icons/${symbol}.svg`);
+      //   } else {
+      //     symbolPath.outerHTML = `<path fill="${symbolColor}" transform="translate(6 6)"
+      //       d="${xml.documentElement.getElementsByTagName("path")[0].getAttribute("d")}"></path>`
+      //   }
+      // });
     }
   }
 
